2fbf0993d2e806918d19159ad56939d14cb9f2e9
Chuvenkova <ladytegira@gmail.com>
2014-04-09 18:00:14 +0000

    Добавлены sql скрипты и документы

---
+++ sql/insert_schema.sql
@@ -0,0 +1,143 @@
+﻿INSERT INTO PAYMENT_TYPE VALUE (
+   NULL, "Оплата");
+    
+INSERT INTO PAYMENT_TYPE VALUE (
+   NULL, "Возврат");
+   
+INSERT INTO PAYMENT_METHOD VALUE (
+   NULL, "Наличные");
+    
+INSERT INTO PAYMENT_METHOD VALUE (
+   NULL, "Карта");
+   
+INSERT INTO CLIENT VALUE (
+   NULL, "Ирина", "Висторовна", "Ермолова", "1985-04-12", "89501234567");
+   
+INSERT INTO CLIENT VALUE (
+   NULL, "Ольга", "Алексеевна", "Иванова", "1987-03-14", "89086547821");
+   
+INSERT INTO CLIENT VALUE (
+   NULL, "Марина", "Игоревна", "Зуева", "1989-11-19", "89513698521");
+
+ INSERT INTO SERVICE_TYPE VALUE (
+   NULL, "Массаж");   
+
+INSERT INTO SERVICE_TYPE VALUE (
+   NULL, "Маникюр");  
+
+INSERT INTO SERVICE_TYPE VALUE (
+   NULL, "Косметолог");    
+
+INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 1, "Массаж спины", "00:40:00");   
+
+INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 1, "Массаж шеи", "00:30:00");  
+
+INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 1, "Массаж тела", "01:30:00");
+
+ INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 2, "Маникюр классический", "01:00:00"); 
+
+ INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 2, "Маникюр Европейский", "01:00:00");   
+
+ INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 2, "Горячий маникюр", "02:00:00"); 
+   
+ INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 2, "Аппаратный маникюр", "02:00:00"); 
+   
+ INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 2, "Наращивание ногтей биогелем", "01:00:00");   
+
+ INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 2, "Коррекция ногтей биогелем", "00:30:00");     
+   
+INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 2, "Покрытие ногтей лаком", "00:30:00"); 
+   
+INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 2, "Акварельная роспись ногтей", "01:00:00");
+
+INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 3, "Чистка лица", "01:30:00");     
+
+INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 3, "Косметический уход за лицом", "00:30:00");
+
+INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 3, "Чистка лица + косметический уход", "02:00:00");
+   
+INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 3, "Вечерний макияж", "00:30:00");  
+
+INSERT INTO SERVICE_SUBTYPE VALUE (
+   NULL, 3, "Обертывание", "00:30:00");    
+ 
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 1, 500);   
+   
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 2, 300); 
+   
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 3, 800);   
+
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 4, 450);   
+
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 5, 450);  
+
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 6, 550);   
+   
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 7, 550);   
+   
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 8, 2500);  
+
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 9, 1500);   
+   
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 10, 100); 
+   
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 11, 600); 
+   
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 12, 1000); 
+   
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 13, 1000); 
+   
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 14, 1500); 
+   
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 15, 800);  
+
+INSERT INTO PRICE_LIST VALUE (
+   NULL, 16, 700);    
+   
+INSERT INTO REGISTRATION VALUE (
+   NULL, 1, 1, "2014-04-04", "10:00:00"); 
+   
+INSERT INTO REGISTRATION VALUE (
+   NULL, 2, 1, "2014-04-04", "11:00:00"); 
+   
+INSERT INTO REGISTRATION VALUE (
+   NULL, 3, 1, "2014-04-04", "12:00:00"); 
+   
+INSERT INTO PAYMENT VALUE (
+   NULL, 1, 1, 1, 500); 
+   
+INSERT INTO PAYMENT VALUE (
+   NULL, 1, 2, 1, 500);   
+
+INSERT INTO PAYMENT VALUE (
+   NULL, 2, 3, 1, 500);     
